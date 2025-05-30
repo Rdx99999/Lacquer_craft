beafdc28ec27e84ae7c1777a2d9a7a7023883b49
hgh hkg <43288520-jaciy24956@users.noreply.replit.com>
2025-05-30 04:45:16 +0000

    Restored to '8e74cec9ecba42ae541edf90b5038bb9d246bfdd'
    Replit-Restored-To: 8e74cec9ecba42ae541edf90b5038bb9d246bfdd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Navigation } from "@/components/navigation";
-import { Footer } from "./components/footer";
 import Home from "./pages/home";
 import Products from "./pages/products";
 import ProductDetail from "./pages/product-detail";
@@ -39,7 +38,6 @@ function App() {
         <div className="min-h-screen bg-warm-cream">
           <Navigation />
           <Router />
-          <Footer />
         </div>
         <Toaster />
       </TooltipProvider>
